87af13e0bc5c11cf4445c58bdd0103f3d48c2b69
Lwandomatiso <86523003+Lwandomatiso@users.noreply.github.com>
2025-11-20 20:54:56 +0200

    Update S3 client to use Amazon credentials to from AWS to AMAZON

--- word_document_server/utils/s3_utils.py
+++ word_document_server/utils/s3_utils.py
@@ -9,9 +9,9 @@ def get_s3_client():
     """Create and return an S3 client with credentials from environment variables."""
     return boto3.client(
         's3',
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        region_name=os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+        aws_access_key_id=os.getenv('AMAZON_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AMAZON_SECRET_ACCESS_KEY'),
+        region_name=os.getenv('AMAZON_DEFAULT_REGION', 'us-east-1')
     )
 
 def generate_presigned_url(bucket_name, object_key, expiration=3600):
